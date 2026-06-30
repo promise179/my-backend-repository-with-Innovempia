1347d823c26884938b843e0799074fede8e5c52e
Promise <Promiseezeoke@gmail.com>
2026-06-30 22:13:54 +0100

    day 3

---
+++ day3.py
@@ -0,0 +1,42 @@
+score = 100 
+print("You scored: " + str(score)) 
+user_age = "25"       
+new_age = int(user_age) + 5 
+print(new_age)      
+whole_num = 10 
+decimal_num = float(whole_num) 
+print(decimal_num)
+print(bool("Hello"))
+print(bool("")) 
+print(bool(0))    
+print(bool('greetings'))
+name = "mOsEs jOhNsOn" 
+
+print(name.upper())
+print(name.lower())       
+print(name.title())       
+print(name.capitalize())
+
+messy = "   hello world   " 
+print(messy.strip())    
+print(messy.lstrip())  
+print(messy.rstrip())
+
+story = "I like cats. My friend likes cats too." 
+print(story.replace("cats", "dogs"))  
+print(story.find("friend"))           
+print(story.count("cats")) 
+
+email = "moses@innovempia.com" 
+print(email.startswith("moses")) 
+print(email.endswith(".com"))     
+user_input = "12345"
+
+print(user_input.isdigit())       
+username = "Moses123" 
+print(username.isalpha())       
+
+word = "Innovempia" 
+print(len(word))  
+input(10)
+
