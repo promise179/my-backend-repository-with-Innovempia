ce4b18db00835ddfe268afa0ee8ac518ad8d4faa
Promise <Promiseezeoke@gmail.com>
2026-07-01 22:52:03 +0100

    corrections

---
+++ correction_quiz day1.py
@@ -0,0 +1,10 @@
+# Q1:Programing is the use of precise lines of code to instruct a computer to inteprete and execute a specific task
+# Q2:Backend is where the main cooking is done
+# Q3:The critical step is to add the phyton.exe to PATH
+# Q4:The three phyton extensions that we installed today are Phyton- code compiler, Pylance- code completer, Phyton indent- code correcter
+# Q5: using the vs code play button, using the vs code integrated terminal, using the system terminal
+# Q6: 
+# Q7: str()
+# Q8: #, """
+# Q9: Git is a local backup, GitHub is a cloud backup
+# Q10: My backend repository with Innovempia
---
+++ correction_quiz day2.py
@@ -0,0 +1,10 @@
+# Q1: save as creates duplicate files which can be lost but it is the opposite for git(commit)
+# Q2: git config --global user.name and git config --global user.email
+# Q3:Repositories are folders tracked by git on a local computer
+# Q4: git commit is to take a snapshot of the code but git push is to upload it 
+# Q5: Origin stands for the cloud server(GitHub)
+# Q6: git revert is  safe to use because it takes bad snapshots of your code and reverts them
+# Q7: git rebase can rewrite the history of the code causing choas
+# Q8: git checkout -b "new branch" 
+# Q9: cloning downloads a copy of the repository while forking creates a server side copy of the repository under your account
+# Q10: Branch protection rules
